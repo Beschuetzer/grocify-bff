e7dd9c79c0afe19d31c4d62175c5dba28e77360c
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-05-07 15:25:04 -0500

    Add dotenv

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
-const PORT = 4000;
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`);
