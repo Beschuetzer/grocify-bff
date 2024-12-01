7ecde15ced224e28c8d40ae4e5eb99ee1d8a910a
Adam <62818816+Beschuetzer@users.noreply.github.com>
2024-12-01 06:11:44 -0600

    fix: issue with default quantity being 0 for processGroceryList

--- src/constants.ts
+++ src/constants.ts
@@ -21,6 +21,8 @@ export const BULK_WRITE_RESULT_DEFAULT = Object.freeze({
   n: 0,
 });
 export const BCRYPT_SALT_ROUND = 10;
+export const DEFAULT_ITEM_NUMBER = 1;
+export const DEFAULT_ITEM_UNIT = 'unit';
 export const ERROR_MSG_NOT_AUTHORIZED = 'You are not authorized to do this';
 export const EMPTY_STRING = '';
 export const EMPTY_NUMBER = 0;
